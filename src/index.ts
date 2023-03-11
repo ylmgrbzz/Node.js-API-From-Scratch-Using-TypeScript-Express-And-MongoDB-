539a86498ca2d235a60aee79c41b85bbe44ba490
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2023-03-11 19:53:23 +0300

    Node.js-API

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,12 @@ import 'module-alias/register';
 import validateEnv from '@/utils/validateEnv';
 import App from './app';
 import PostController from '@/resources/post/post.controller';
-// import UserController from '@/resources/user/user.controller';
+import UserController from '@/resources/user/user.controller';
 
 validateEnv();
 
 const app = new App(
-    [new PostController(),
-        //  new UserController()
-        ],
+    [new PostController(), new UserController()],
     Number(process.env.PORT)
 );
 
